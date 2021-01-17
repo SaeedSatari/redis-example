03a1325068ab87530dfd4b5caf9167aa283acd71
SaeedSatari <saeedsatari93@gmail.com>
2021-01-17 16:13:41 +0330

    initial commit

---
+++ src/test/java/ir/ssatari/redis/RedisExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.ssatari.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
