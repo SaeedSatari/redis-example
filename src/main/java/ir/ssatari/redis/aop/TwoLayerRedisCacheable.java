9970f055295f75c77c991bfffdf6008697b933d1
Saeid Satari <S.Satari@sadad.co.ir>
2021-01-18 09:03:15 +0330

    added aop

---
+++ src/main/java/ir/ssatari/redis/aop/TwoLayerRedisCacheable.java
@@ -0,0 +1,14 @@
+package ir.ssatari.redis.aop;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TwoLayerRedisCacheable {
+    long firstLayerTtl() default 10L;
+    long secondLayerTtl() default 60L;
+    String key();
+}
