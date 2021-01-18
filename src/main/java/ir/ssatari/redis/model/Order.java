fdd5de9f1c7128c0c2800a61ba316e319661b9d5
Saeid Satari <S.Satari@sadad.co.ir>
2021-01-18 09:04:43 +0330

    added Order model

---
+++ src/main/java/ir/ssatari/redis/model/Order.java
@@ -0,0 +1,14 @@
+package ir.ssatari.redis.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+public class Order implements Serializable {
+    private static final long serialVersionUID = -3281088656501163895L;
+    private int id;
+    private long orderNumber;
+}
