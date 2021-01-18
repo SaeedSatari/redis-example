32820d4177df782ca7b454e1a81ce19b6dca0012
Saeid Satari <S.Satari@sadad.co.ir>
2021-01-18 09:06:58 +0330

    added Customer model

---
+++ src/main/java/ir/ssatari/redis/model/Customer.java
@@ -0,0 +1,16 @@
+package ir.ssatari.redis.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+public class Customer implements Serializable {
+
+    private static final long serialVersionUID = 6076109187134928213L;
+    private int id;
+    private String firstName;
+    private String lastName;
+}
